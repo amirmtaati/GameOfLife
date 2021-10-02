088532b8ac5c75b070f8f40f32b49119dc394609
amirMohammadTaati <mamad198585@gmail.com>
2021-10-02 18:01:59 +0330

    global variables added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 const canvas  = document.getElementById("canvas");
 const context = canvas.getContext('2d');
+let grid;
+
+const CELL_SIZE     = 10;
+const CANVAS_WIDTH  = 900;
+const CANVAS_HEIGHT = 900;
+
+const GRID_WIDTH  = CANVAS_WIDTH  / CELL_SIZE;
+const GRID_HEIGHT = CANVAS_HEIGHT / CELL_SIZE;
 
