4baf74f8a65ba6b8c68df5b1ec27f66f6d9c6429
amirMohammadTaati <mamad198585@gmail.com>
2021-10-02 18:08:38 +0330

    in this case , OOP sounds good : )

--- src/main.js
+++ src/main.js
@@ -22,6 +22,25 @@ function makeGrid() {
     return grid;
 }
 
+class Grid {
+    constructor(width , height) {
+        this.width = width;
+        this.height = height;
+        this.grid = [];
+
+        this.initGrid();
+    }
+
+    initGrid() {
+        for (let x = 0 ; x < this.width ; x++) {
+            for (let y = 0 ; y < this.height ; y++) {
+                this.grid[x] = new Array(this.height).fill(0);
+            }
+        }
+    }
+}
+
 (function main() {
+    const world = new Grid(GRID_WIDTH , GRID_HEIGHT);
 
 })();
