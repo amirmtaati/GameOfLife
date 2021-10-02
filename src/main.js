f06f50b3902001c2104c65441b396c53e0485ca0
amirMohammadTaati <mamad198585@gmail.com>
2021-10-02 18:02:44 +0330

    makeGrid function added

--- src/main.js
+++ src/main.js
@@ -10,6 +10,17 @@ const GRID_WIDTH  = CANVAS_WIDTH  / CELL_SIZE;
 const GRID_HEIGHT = CANVAS_HEIGHT / CELL_SIZE;
 
 
+function makeGrid() {
+    let grid = [];
+
+    for (let x = 0 ; x < GRID_WIDTH ; x++) {
+        for (let y = 0 ; y < GRID_HEIGHT ; y++) {
+            grid[y] = new Array(GRID_WIDTH);
+        }
+    }
+
+    return grid;
+}
 
 (function main() {
 
