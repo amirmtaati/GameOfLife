18affbd848c88444a7eb79aaee0756b639243b4c
amirMohammadTaati <mamad198585@gmail.com>
2021-10-02 18:25:48 +0330

    draw method added to Grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,9 @@
 export default class Grid {
-    constructor(width , height) {
+    constructor(width , height , cellSise , context) {
         this.width = width;
         this.height = height;
+        this.cellSize = cellSise;
+        this.context = context;
         this.grid = [];
 
         this.initGrid();
@@ -11,6 +13,15 @@ export default class Grid {
         for (let x = 0 ; x < this.width ; x++) {
             for (let y = 0 ; y < this.height ; y++) {
                 this.grid[x] = new Array(this.height).fill(0);
+            }
+        }
+    }
+
+
+    draw() {
+        for (let x = 0 ; x < this.width ; x++) {
+            for (let y = 0 ; y < this.height ; y++) {
+                this.context.fillRect(this.cellSize * x , this.cellSize * y , this.cellSize - 1 , this.cellSize - 1);
             }
         }
     }
