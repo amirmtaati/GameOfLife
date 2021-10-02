a35c69f20955400b3b925993e924cfd6ec386192
amirMohammadTaati <mamad198585@gmail.com>
2021-10-02 18:29:19 +0330

    making world in main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import Grid from "./Grid.js";
 
 const canvas  = document.getElementById("canvas");
 const context = canvas.getContext('2d');
-let grid;
 
-const CELL_SIZE     = 10;
+const CELL_SIZE     = 20;
 const CANVAS_WIDTH  = 900;
 const CANVAS_HEIGHT = 900;
 
@@ -14,5 +13,7 @@ const GRID_HEIGHT = CANVAS_HEIGHT / CELL_SIZE;
 
 
 (function main() {
-    const world = new Grid(GRID_WIDTH , GRID_HEIGHT);
+    const world = new Grid(GRID_WIDTH , GRID_HEIGHT , CELL_SIZE , context);
+
+    world.draw();
 })();
