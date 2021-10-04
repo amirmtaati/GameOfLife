e2241eaa5df49b051a438fe43dd04e96d58d3e3e
amirMohammadTaati <mamad198585@gmail.com>
2021-10-04 11:02:50 +0330

    drawing method changed

--- src/Grid.js
+++ src/Grid.js
@@ -1,3 +1,5 @@
+import { Cell } from './Cell.js';
+
 export default class Grid {
     constructor(width , height , cellSise , context) {
         this.width = width;
@@ -12,19 +14,27 @@ export default class Grid {
     initGrid() {
         for (let x = 0 ; x < this.width ; x++) {
             for (let y = 0 ; y < this.height ; y++) {
-                this.grid[x] = new Array(this.height).fill(0);
+                this.grid[x] = new Array(this.height);
             }
         }
+
+        this.fillGrid();
     }
 
-    drawCell(x , y) {
-        this.context.fillRect(this.cellSize * x , this.cellSize * y , this.cellSize - 1 , this.cellSize - 1);
+    fillGrid() {
+        for (let x = 0 ; x < this.width ; x++) {
+            for (let y = 0 ; y < this.height ; y++) {
+                const cell = new Cell(x , y , this.cellSize , this.context);
+                this.grid[x][y] = cell;
+            }
+        }
+        console.log(this.grid);
     }
 
     draw() {
         for (let x = 0 ; x < this.width ; x++) {
             for (let y = 0 ; y < this.height ; y++) {
-                this.drawCell(x , y);
+                this.grid[x][y].drawCell();
             }
         }
     }
