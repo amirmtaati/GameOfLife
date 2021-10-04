3b61eaae84d8eb7625b176ad74e842cf79368c12
amirMohammadTaati <mamad198585@gmail.com>
2021-10-04 11:05:09 +0330

    error fixed

--- src/Grid.js
+++ src/Grid.js
@@ -1,41 +1,41 @@
-import { Cell } from './Cell.js';
+import Cell from "./Cell.js";
 
 export default class Grid {
-    constructor(width , height , cellSise , context) {
-        this.width = width;
-        this.height = height;
-        this.cellSize = cellSise;
-        this.context = context;
-        this.grid = [];
+  constructor(width, height, cellSise, context) {
+    this.width = width;
+    this.height = height;
+    this.cellSize = cellSise;
+    this.context = context;
+    this.grid = [];
 
-        this.initGrid();
+    this.initGrid();
+  }
+
+  initGrid() {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        this.grid[x] = new Array(this.height);
+      }
     }
 
-    initGrid() {
-        for (let x = 0 ; x < this.width ; x++) {
-            for (let y = 0 ; y < this.height ; y++) {
-                this.grid[x] = new Array(this.height);
-            }
-        }
+    this.fillGrid();
+  }
 
-        this.fillGrid();
+  fillGrid() {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        const cell = new Cell(x, y, this.cellSize, this.context);
+        this.grid[x][y] = cell;
+      }
     }
+    console.log(this.grid);
+  }
 
-    fillGrid() {
-        for (let x = 0 ; x < this.width ; x++) {
-            for (let y = 0 ; y < this.height ; y++) {
-                const cell = new Cell(x , y , this.cellSize , this.context);
-                this.grid[x][y] = cell;
-            }
-        }
-        console.log(this.grid);
+  draw() {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        this.grid[x][y].drawCell();
+      }
     }
-
-    draw() {
-        for (let x = 0 ; x < this.width ; x++) {
-            for (let y = 0 ; y < this.height ; y++) {
-                this.grid[x][y].drawCell();
-            }
-        }
-    }
+  }
 }
