c05270c7d1f7b65e28cd4ae2d2f2384195148fec
amirMohammadTaati <mamad198585@gmail.com>
2021-10-06 16:58:25 +0330

    width and height changed in main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const canvas  = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
 const CELL_SIZE     = 20;
-const CANVAS_WIDTH  = 900;
-const CANVAS_HEIGHT = 900;
+const CANVAS_WIDTH  = 700;
+const CANVAS_HEIGHT = 500;
 
 const GRID_WIDTH  = CANVAS_WIDTH  / CELL_SIZE;
 const GRID_HEIGHT = CANVAS_HEIGHT / CELL_SIZE;
